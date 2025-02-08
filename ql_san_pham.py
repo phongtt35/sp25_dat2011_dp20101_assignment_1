2b50dc5a072453dbd66ba5944f2138d732938672
TungTV90 <tungtv205@gmail.com>
2025-02-08 15:45:28 +0700

    Update ql_san_pham.py

--- ql_san_pham.py
+++ ql_san_pham.py
@@ -1,12 +1,23 @@
 # Chương trình con
 # Quản lý sản phẩm của công ty
 
-def menu(a):
-    print('Thêm sản phẩm')
-    print('Xóa sản phẩm')
-    print('Sửa đơn giá sản phẩm')
-    print('Thêm sản phẩm')
-
 def ql_san_pham():
     print("Chương trình con")
     print("Quản lý sản phẩm của công ty")
+    while(True):
+        choice = input()
+        print('1. Thêm sản phẩm')
+        print('2. Xóa sản phẩm')
+        print('3. Sửa sản phẩm')
+        print('0. Thoát')
+        if choice == '1':
+            print('Đã thêm sản phẩm')
+        elif choice == '2':
+            print('Đã xóa sản phẩm')
+        elif choice == '3':
+            print('Đã sửa sản phẩm')
+        elif choice == '0':
+            break
+        else:
+            print('Không có sự lựa chọn, Again!')
+
