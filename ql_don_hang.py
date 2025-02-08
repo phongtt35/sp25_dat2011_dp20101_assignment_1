db98618d45a714841410dc0e7857fb9aec8e9fca
Trdngx <hatridung1610@gmail.com>
2025-02-08 15:07:28 +0700

    Update ql_don_hang.py
    create menu

--- ql_don_hang.py
+++ ql_don_hang.py
@@ -1,6 +1,12 @@
 # Chương trình con
 # Quản lý đơn hàng của khách hàng
 
+def menu():
+    print('1. Thêm đơn hàng')
+    print('2. Sửa đơn hàng')
+    print('3. Xóa đơn hàng')
+    print('4. Thoát đơn hàng')
+
 def ql_don_hang():
     print("Chương trình con")
     print("Quản lý đơn hàng của khách hàng")
