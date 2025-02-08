9bc993b0b028e12c0d7bac3805230c1e19bc3cff
TungTV90 <tungtv205@gmail.com>
2025-02-08 14:57:41 +0700

    Update ql_san_pham.py

--- ql_san_pham.py
+++ ql_san_pham.py
@@ -1,7 +1,12 @@
 # Chương trình con
 # Quản lý sản phẩm của công ty
 
+def menu(a):
+    print('Thêm sản phẩm')
+    print('Xóa sản phẩm')
+    print('Sửa đơn giá sản phẩm')
+    print('Thêm sản phẩm')
+
 def ql_san_pham():
     print("Chương trình con")
     print("Quản lý sản phẩm của công ty")
-    print("")
