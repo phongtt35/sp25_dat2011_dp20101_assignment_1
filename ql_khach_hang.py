6283840a8378f646d9d77b2e201acc7c33f20ec0
ngobaobao <huyoicolen00@gmail.com>
2025-02-08 16:10:06 +0700

    Update ql_khach_hang.py

--- ql_khach_hang.py
+++ ql_khach_hang.py
@@ -1,35 +1,57 @@
 # Chương trình con
 # Quản lý khách hàng của công ty
 
-def them_don_hang_khach_hang():
-    print("Thêm đơn hàng khách hàng. ")
-    
-def sua_don_hang_khach_hang():
-    print("Sửa đơn hàng khách hàng. ")
-    
-def xoa_don_hang_khach_hang():
-    print("Xóa đơn hàng khách hàng. ")
 
+def menu():
+    while True:
+        print('1.Quản lý khách hàng.')
+        print('2. Thoát')
+        choice = input('Chọn chức năng: ')
+        if choice == '1':
+            ql_khach_hang()
+        elif choice == '2':
+            print('Thoát chương trình')
+            break
+        else:
+            print('Lựa chọn không hợp lệ')
 
 def ql_khach_hang():
     while True:
-        print("\nChương trình quản lý đơn hàng khách hàng")
-        print("1. Thêm đơn hàng khách hàng")
-        print("2. Sửa đơn hàng khách hàng")
-        print("3. Xóa đơn hàng khách hàng")
-        print("4. Thoát")
+        print('\n--- Quản lý Khách Hàng ---')
+        print('1. Danh sách khách hàng')
+        print('2. Thêm khách hàng')
+        print('3. Xóa khách hàng')
+        print('4. Cập nhật thông tin khách hàng')
+        print('5. Tìm kiếm khách hàng')
+        print('6. Quay lại menu chính')
         
-        chon = int(input("Chọn chức năng: "))
-        
-        if chon == 1:
-            them_don_hang_khach_hang()
-        elif chon == 2:
-            sua_don_hang_khach_hang()
-        elif chon == 3:
-            xoa_don_hang_khach_hang()
-        elif chon == 4:
+        choice = input('Chọn chức năng: ')
+        if choice == "1":
+            danh_sach()
+        elif choice == "2":
+            them_kh()
+        elif choice == "3":
+            xoa_kh()
+        elif choice == "4":
+            cap_nhat()
+        elif choice == "5":
+            tim()
+        elif choice == "6":
             break
         else:
-            print("Chọn chức năng không hợp lệ. ")
+            print("Lựa chọn không hợp lệ, vui lòng nhập lại.")
             
-ql_khach_hang()
+def danh_sach():
+    print('Danh sách khách hàng')
+def them_kh():
+    print('Thêm khách hàng')
+def xoa_kh():
+    print('Xóa khách hàng')
+def cap_nhat():
+    print('Cập nhật thông tin khách hàng')
+def tim():
+    print('Tìm khách hàng')
+
+menu()
+
+
