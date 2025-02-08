ee0fd5545f75864b45b86d23ecca13d2c183ff40
Trdngx <hatridung1610@gmail.com>
2025-02-08 17:03:47 +0700

    Update_v2
    update

--- ql_don_hang.py
+++ ql_don_hang.py
@@ -7,12 +7,13 @@ def ql_don_hang():
     print("Quản lý đơn hàng của khách hàng")
     print('-------------------')
     while(True):
-        choice = input()
         print('1. Thêm đơn hàng')
         print('2. Xem danh sách đơn hàng')
         print('3. Sửa đơn hàng')
         print('4. Xóa đơn hàng')
         print('5. Thoát đơn hàng')
+        
+        choice = input('chọn chức năng: ')
         if choice == '1':
             print('đã thêm 1')
         elif choice == '2':
@@ -21,5 +22,8 @@ def ql_don_hang():
             print('đã thêm 3')
         elif choice == '4':
             print('đã thêm 4')
-        else:
+        elif choice == '5':
             print('thoát')
+            break
+        else:
+            print('chọn lại !!!')
