99608dacc62ede9da07b1b86ef15ac0ab89fd41f
HSon4 <anhsontp666@gmail.com>
2025-02-09 08:54:12 +0700

    Update ql_phieu_giam_gia.py

--- ql_phieu_giam_gia.py
+++ ql_phieu_giam_gia.py
@@ -1,5 +1,18 @@
 # Chương trình con
 # Quản lý công việc của từng nhân viên
+
+def them_ma():
+    print("Đã thêm mã")
+
+def sua_ma():
+    print("Đã sửa mã")
+
+def xoa_ma():
+    print("Đã xoá mã")  
+
+def xem_ma():
+    print("Đã xem mã")
+
 def ql_phieu_giam_gia():
     print("Chương trình con")
     print("Quản lý công việc của từng nhân viên")
@@ -8,7 +21,7 @@ def ql_phieu_giam_gia():
         print("\t1. Thêm mã giảm giá")
         print("\t2. Sửa mã giảm giá")
         print("\t3. Xoá mã giảm giá")
-        print("\t4. xem mã giảm giá")
+        print("\t4. Xem mã giảm giá")
         print("\t0. Thoát")
 
         lua_chon = input("Nhập lựa chọn của bạn: ")
@@ -24,15 +37,8 @@ def ql_phieu_giam_gia():
             print("Đã thoát")
             break
         else:
-            print("Lựa chọn không hợp lệ Vui lòng thử lại!")
+            print("Lựa chọn không hợp lệ, vui lòng thử lại!")
             print('\n')
-ql_phieu_giam_gia()
 
-def them_ma():
-    print("Đã thêm mã")
-def sua_ma():
-    print("Đã sửa mã")
-def xoa_mã():
-    print("Đã xoá Mã")
-def xem_ma():
-    print("Đã xem mã")
+if __name__ == "__main__": 
+    ql_phieu_giam_gia()
